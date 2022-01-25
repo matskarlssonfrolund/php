312aa46c3f1060125a5202f2e6cd4fb17d0c7ad5
matskarlssonfrolund <60854799+matskarlssonfrolund@users.noreply.github.com>
2022-01-25 23:06:54 +0100

    Add files via upload

--- Login_system_new/header.php
+++ Login_system_new/header.php
@@ -8,10 +8,13 @@
   <head>
     <meta charset="utf-8">
     <title>PHP Project 01</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!--I won't do more than barebone HTML, since this isn't an HTML tutorial.-->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/style1.css">
   </head>
   <body>
 
@@ -20,12 +23,12 @@
       <div class="wrapper">
         <a href="index.php"><img src="img/logo-white.png" alt="Blogs logo"></a>
         <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="discover.php">About Us</a></li>
-          <li><a href="blog.php">Find Blogs</a></li>
+          <li><a href="index.php">Start</a></li>
+          <li><a href="discover.php">Om företaget</a></li>
+          <li><a href="blog.php">Blog</a></li>
           <?php
             if (isset($_SESSION["useruid"])) {
-              echo "<li><a href='profile.php'>Profile Page</a></li>";
+              echo "<li><a href='profile.php'>Medlemssida</a></li>";
               echo "<li><a href='logout.php'>Logout</a></li>";
             }
             else {
