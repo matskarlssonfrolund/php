b93d2699c71272bc5ba0fdec295c682e029341f6
matskarlssonfrolund <60854799+matskarlssonfrolund@users.noreply.github.com>
2022-01-25 23:05:28 +0100

    Add files via upload

--- Login_system_new/index.php
+++ Login_system_new/index.php
@@ -4,7 +4,7 @@
 ?>
 
 <section class="index-intro">
-  <h1>This Is An Introduction</h1>
+  <h1>MCSON Silver & Sång</h1>
   <p>Here is an important paragraph that explains the purpose of the website and why you are here!</p>
 </section>
 
@@ -12,16 +12,16 @@
   <h2>Some Basic Categories</h2>
   <div class="index-categories-list">
     <div>
-      <h3>Fun Stuff</h3>
+      <h3>Ringar</h3>
     </div>
     <div>
-      <h3>Serious Stuff</h3>
+      <h3>Armband</h3>
     </div>
     <div>
-      <h3>Exciting Stuff</h3>
+      <h3>Örhängen</h3>
     </div>
     <div>
-      <h3>Boring Stuff</h3>
+      <h3>Annat</h3>
     </div>
   </div>
 </section>
